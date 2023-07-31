25c658a39f61003c6447f1dd61173385f1f5ac78
Jujhar Singh <jujhar@jujhar.com>
2023-07-31 09:32:12 +0100

    removes console log and comments

--- src/js/cardModel.js
+++ src/js/cardModel.js
@@ -1,40 +1,9 @@
 // transform the raw card json
-// we want an array of elements that look like
-// [{
-//   categoryName: <name>
-//   cards:[<card_data>,card_data>]
-// },
-// {
-//   categoryName: <name>
-//   cards:[<card_data>,card_data>]
-// }]
 import rawCardData from "../data/cards.json";
 
 const cardData = rawCardData.map((card) => {
-  if (card?.moreInfoLinks) {
-    console.log(card?.moreInfoLinks);
-  }
+  // do any data manipulation here
   return card;
 });
-// get all categories first then add data to them
-// we want the data objects by returned by category
-// get unique categories to begin with
-// let categories = {};
-// rawCardData.forEach((el) => {
-//   categories[el.category] = {};
-// });
-// rawCardData.forEach((card) => {
-//   if (card.category) {
-//     categories[card.category].categoryName = card.category;
-//     categories[card.category].cards = [];
-//   }
-//   if (card.category) {
-//     categories[card.category].categoryName = card.category;
-//     categories[card.category].cards.push(card);
-//   }
-// });
-
-// const cardOnlyData = Object.values(categories);
-// console.log(cardOnlyData);
 
 export default cardData;
